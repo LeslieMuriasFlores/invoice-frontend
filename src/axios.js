019a533dc24b0ac9f1f464c903954767e8cfa612
lesliemf <76230407+LeslieMuriasFlores@users.noreply.github.com>
2024-10-17 23:37:31 +0200

    ..ssl

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://nemon-production.up.railway.app/api',
+    baseURL: 'https://nemon-production.up.railway.app/api',
     headers: {
         'Content-Type': 'application/json',
       },
