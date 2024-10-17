67f6d5026ae6c9b5be48f78a23d7a13cf417fa3b
lesliemf <76230407+LeslieMuriasFlores@users.noreply.github.com>
2024-10-17 23:30:53 +0200

    change host

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: 'invoice-frontend-production.up.railway.app', 
+    host: '0.0.0.0', 
     port: 5173, // El puerto que estás usando
     strictPort: true,
   },
