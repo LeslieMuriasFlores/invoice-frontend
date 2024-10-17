047c7879b62b1181e815c69dbcaa1c79d83904c8
lesliemf <76230407+LeslieMuriasFlores@users.noreply.github.com>
2024-10-17 23:35:02 +0200

    add url production

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://nemon.test:80/api',
+    baseURL: 'http://nemon-production.up.railway.app/api',
     headers: {
         'Content-Type': 'application/json',
       },
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', 
-    port: 5173, // El puerto que estás usando
+    port: 5173,
     strictPort: true,
   },
 })
